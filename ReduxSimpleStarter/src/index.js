d8726b84e7c8d75f8333bf78ee20ec3490e30b71
wilsonandiphone@icloud.com <wilsonandiphone@icloud.com>
2017-05-23 23:41:05 -0400

    add search bar

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyC6_MNLd8yop2pwWbz1EnREMYQCkKwPQmo';
 
 // Create a new component, this component should produce some HTML
 const App = () => {
-  return <div>Wilson</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it on the page
