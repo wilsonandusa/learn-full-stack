7a6c243d69f04160ad6cc6408b8dce86d103e60d
wilsonandiphone@icloud.com <wilsonandiphone@icloud.com>
2017-05-30 23:08:03 -0400

    add vue example

---
+++ vue/vueapp01/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Hello></Hello>
+    <greeting></greeting>
+    <T_Plot></T_Plot>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import T_Plot from './components/T_Plot'
+export default {
+ name: 'app',
+  components: {
+    Hello,
+    T_Plot
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vueapp01/src/components/T_Plot.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    I am {{name}}  <button v-on:click="changeName">change</button>
+    </div>
+</template>
+
+<script>
+
+export default{
+  name: 'wilson',
+  data: function(){
+    return {
+      name: 'wilson'
+    }
+  },
+  methods: {
+    changeName:function(){
+      this.name = 'xiaoshengwu';
+    }
+  }
+}
+
+</script>
